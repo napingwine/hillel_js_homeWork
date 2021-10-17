cde0c431ec632c9f03f1bbc95d22b15a4eae17c4
Andrii Pymonov <andriipymonov@gmail.com>
2021-10-17 16:52:29 +0300

    hw7 debug

--- homeWork 7 - Pymonov/js/app.js
+++ homeWork 7 - Pymonov/js/app.js
@@ -1,15 +1,28 @@
-let pName = prompt('Please enter person name');
+let pName;
 let pAge;
-pAge = isItNumber(pAge, 'person age');
-let brand = prompt('Please enter car brand');
+let brand;
 let productionYear;
+let horsePower;
+let color;
+let bodyType;
+
+do{
+  pName = prompt('Please enter person name');
+}while(!isItEmpty(pName))
+pAge = isItNumber(pAge, 'person age');
+do{
+  brand = prompt('Please enter car brand');
+}while(!isItEmpty(brand))
 do{
   productionYear = isItNumber(productionYear, 'year production of car'); 
 }while(!isSomeCarsWasProduced(productionYear))
-let horsePower;
 horsePower = isItNumber(horsePower, 'car horse power');
-let color = prompt('Please enter car color');
-let bodyType = prompt('Please enter car body type');
+do{
+  color = prompt('Please enter car color');
+}while(!isItEmpty(color))
+do{
+  bodyType = prompt('Please enter car body type');
+}while(!isItEmpty(bodyType))
 
 const user = new Person(pName, pAge)
 const userCar = new Car(brand, productionYear, horsePower, color, bodyType)
--- homeWork 7 - Pymonov/js/functions.js
+++ homeWork 7 - Pymonov/js/functions.js
@@ -6,7 +6,7 @@ function isItNumber(number, text) {
 }
 
 function addOwnerIfAgeIsSufficient(carName, user){
-  if(carName > 18){
+  if(user.age > 18){
     carName.addOwner(user)
   }else{
     console.log('Your age must be more than 18')
@@ -16,6 +16,15 @@ function addOwnerIfAgeIsSufficient(carName, user){
 function isSomeCarsWasProduced(productionYear){
   if(Number(productionYear) < 1885){
     alert('first car was produced at 1885, are you sure? please check car year production and enter again')
+  }else{
+    return true
   }
-  return true
+}
+
+function isItEmpty(variable){
+  if(variable === '' || variable === undefined ){
+    alert('Please fillup all fields!')
+  }else{
+    return true
+  }
 }
