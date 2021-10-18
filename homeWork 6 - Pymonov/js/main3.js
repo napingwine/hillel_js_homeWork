291f04e653b53c8cfc00579c53e504438ce77159
Andrii Pymonov <andriipymonov@gmail.com>
2021-10-18 18:49:38 +0300

    hw6 debug task 3

--- homeWork 6 - Pymonov/js/main3.js
+++ homeWork 6 - Pymonov/js/main3.js
@@ -3,32 +3,34 @@ let number2;
 let znak;
 let result;
 
-do{
+do {
   number1 = prompt('please enter 1st number');
-}while(isNaN(number1))
+} while (isNaN(number1))
 
-do{
+do {
   number2 = prompt('please enter 2nd number');
-}while(isNaN(number2))
+} while (isNaN(number2))
 
 do {
   znak = prompt('please enter operator(+,-,*,/,% or ^)');
 } while (znak !== '+' && znak !== '-' && znak !== '*' && znak !== '/' && znak !== '%' && znak !== '^')
 
+number1 = Number(number1);
+number2 = Number(number2);
+
 function doMath(number1, number2, znak) {
   if (znak === '+') {
-    console.log(1)
-    result = (Number(number1) + Number(number2))
+    result = number1 + number2;
   } else if (znak === '-') {
-    result = (Number(number1) - Number(number2))
+    result = number1 - number2;
   } else if (znak === '*') {
-    result = (Number(number1) * Number(number2))
+    result = number1 * number2;
   } else if (znak === '/') {
-    result = (Number(number1) / Number(number2))
+    result = number1 / number2;
   } else if (znak === '%') {
-    result = (Number(number1) % Number(number2))
+    result = number1 % number2;
   } else if (znak === '^') {
-    result = pow(Number(number1), Number(number2))
+    result = pow(number1, number2);
   }
 }
 
@@ -39,6 +41,6 @@ function pow(x, n) {
     return x * pow(x, n - 1);
   }
 }
-doMath(number1, number2, znak)
+doMath(number1, number2, znak);
 
-console.log(`${number1}${znak}${number2}=${result}`)
+console.log(`${number1}${znak}${number2}=${result}`);
