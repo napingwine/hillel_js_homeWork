cc7ff18c7869677a892545197ec0919d28893152
Andrii Pymonov <andriipymonov@gmail.com>
2021-10-20 12:47:18 +0300

    app debug

--- homeWork 7 - Pymonov/js/app.js
+++ homeWork 7 - Pymonov/js/app.js
@@ -6,6 +6,30 @@ let horsePower;
 let color;
 let bodyType;
 
+do {
+  pName = prompt('Please enter person name');
+} while (isEmpty(pName));
+
+pAge = getNumber('person age');
+
+do {
+  brand = prompt('Please enter car brand');
+} while (isEmpty(brand));
+
+do {
+  productionYear = getNumber('year production of car');
+} while (isSomeCarsWasProduced(productionYear));
+
+horsePower = getNumber('car horse power');
+
+do {
+  color = prompt('Please enter car color');
+} while (isEmpty(color));
+
+do {
+  bodyType = prompt('Please enter car body type');
+} while (isEmpty(bodyType));
+
 const user = new Person(pName, pAge);
 const userCar = new Car(brand, productionYear, horsePower, color, bodyType);
 addOwnerIfAgeIsSufficient(userCar, user);
