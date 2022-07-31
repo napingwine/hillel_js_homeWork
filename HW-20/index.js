fa88ab3b440467a3e88f87bcbde01ef5afafb4f1
Andrii Pymonov <andriipymonov@gmail.com>
2022-07-31 22:38:33 +0300

    Добавленна возможность изменения ласт итема

--- HW-20/index.js
+++ HW-20/index.js
@@ -1,13 +1,15 @@
 const root = document.querySelector('.root')
 
 const setClass = (list, level, newClass) => {
-  level--
+  if(typeof level == 'number')level--
   let ulList = [...list.children];
+  if(level == 'last') level = ulList.length-1
+  console.log(level)
   ulList[level].classList.add(newClass);
   ulList[level].classList.add('animation');
 };
 
 setTimeout(() => {
   setClass(root, 1, 'first')
-  setClass(root, 3, 'last')
+  setClass(root, 'last', 'last')
 }, 2000)
